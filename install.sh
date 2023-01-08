f07cb9fdc75579956cb35368bdbec51d1a4d463f
ArsFy <arsfy@outlook.com>
2023-01-08 13:43:12 +0800

    add sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt install wget
+wget https://github.com/ArsFy/edge-state/releases/download/1.0/edge-state
+chmod 777 edge-state
+cat > /etc/systemd/system/edge-state.service <<EOF
+[Unit]
+Description=edge-state
+[Service]
+Type=simple
+WorkingDirectory=/root/
+ExecStart=$pwd/edge-state
+Restart=always
+RestartSec=5
+StartLimitInterval=3
+RestartPreventExitStatus=137
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+service edge-state start
